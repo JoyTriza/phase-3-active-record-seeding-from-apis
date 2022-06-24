d188e58d984c0d42e5c12ab086d6589f7893aa01
Trizah <wangarijoytriza@gmail.com>
2022-06-24 20:33:17 +0300

    Done

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,3 +1,15 @@
 class Spell < ActiveRecord::Base
+    puts "Seeding spells..."
 
+    spells.each do |spell|
+        response = RestClient.get "https://www.dnd5eapi.co/api/spells/#{spell}"
+
+        spell_hash = JSON.parse(response)
+        Spell.create(
+        name: spell_hash["name"],
+        level: spell_hash["level"],
+        description: spell_hash["desc"][0]
+  )
 end
+
+puts "Done Seeding!"
